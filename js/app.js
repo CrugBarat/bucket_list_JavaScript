46a9ae16f8eb9d3c37a5cef6ecb0757ece10585d
Craig Barratt <craigtbarratt@gmail.com>
2020-04-18 13:16:22 +0100

    adds js for form

---
+++ js/app.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  const newForm = document.querySelector('#beach-form');
+  newForm.addEventListener('submit', handleNewForm);
+
+});
+
+const handleNewForm = function (event) {
+  event.preventDefault();
+  const newForm = createNew(event.target);
+  const formInput = document.querySelector('#beach-list');
+  formInput.appendChild(newForm);
+  event.target.reset();
+}
+
+const createNew = function (form) {
+
+  const newForm = document.createElement('li');
+  newForm.classList.add('cell');
+
+  const beach = document.createElement('h2');
+  beach.textContent = form.beach.value;
+  newForm.appendChild(beach);
+
+  const city = document.createElement('h3');
+  city.textContent = form.city.value;
+  newForm.appendChild(city);
+
+  const country = document.createElement('p');
+  country.textContent = form.country.value;
+  newForm.appendChild(country);
+
+  return newForm;
+
+}
